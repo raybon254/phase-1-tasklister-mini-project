d27025bde79b6417f26145f8b3bca0c011be6b88
Brian <musyimib449@gmail.com>
2025-03-15 16:57:07 +0300

    updated js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    toDo()
+    form.reset();
+  })
 });
+
+function toDo(){
+    const desc = document.getElementById('description').value.trim()
+    console.log(desc)
+
+    const p = document.createElement('p')  
+    const ul = document.createElement('ul')
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+
+    btn.addEventListener('click', ()=>{
+        btn.parentNode.remove()
+    })
+
+    btn.textContent = 'X'
+    li.textContent = desc
+    btn.style.marginLeft = '5px'
+
+
+    li.appendChild(btn)
+    ul.appendChild(li)
+    p.appendChild(ul)
+    
+    
+    
+    const display = document.querySelector('#list')
+    display.appendChild(p)
+}
+
